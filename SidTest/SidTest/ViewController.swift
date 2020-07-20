397fc760686960c0f006b3004b9d98a258d0a6da
Siddharth Daga <siddhudaga007@gmail.com>
2020-07-20 22:07:03 +0530

    Porject Created with Core Data

---
+++ SidTest/SidTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SidTest
+//
+//  Created by Backup on 20/07/20.
+//  Copyright © 2020 Backup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
